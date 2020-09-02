f09e7e4eb5675015f354fb165ce7ccef80fdaf96
ghazale <ghazalnoroozi27@yahoo.com>
2020-09-02 19:39:23 +0430

    Fix photo and status and bio

--- Main.py
+++ Main.py
@@ -1,42 +1,36 @@
 from telethon import TelegramClient, events, sync
 from telethon.tl import functions
+from telethon.tl.functions.users import GetFullUserRequest
 from telethon.errors import SessionPasswordNeededError
 import Config
 
 
 def harvest(username):
-    api_id = Config.api_id
-    api_hash = Config.api_hash
-    phone = Config.phone
-    user_id = Config.user_id
-    client = TelegramClient(user_id, api_id, api_hash)
+    host_api_id = Config.api_id
+    host_api_hash = Config.api_hash
+    host_user_id = Config.user_id
+    client = TelegramClient(host_user_id, host_api_id, host_api_hash)
     client.start()
 
-    entity = client.get_entity(username)
-    print("id: ", entity.__getattribute__("id"))
-    print("access_hash: ", entity.__getattribute__("access_hash"))
-    print("first_name: ", entity.__getattribute__("first_name"))
-    print("last_name: ", entity.__getattribute__("last_name"))
-    print("phone: ", entity.__getattribute__("phone"))
-    print("status: ", entity.__getattribute__("status").stringify())
-    print("photo", entity.__getattribute__("photo"))
+    # entity = client.get_entity(username)
+    entity = client(GetFullUserRequest(username))
+    user = entity.__getattribute__("user")
+    print(entity)
+    print("id: ", user.__getattribute__("id"))
+    print("access_hash: ", user.__getattribute__("access_hash"))
+    print("first_name: ", user.__getattribute__("first_name"))
+    print("last_name: ", user.__getattribute__("last_name"))
+    print("phone: ", user.__getattribute__("phone"))
+    print("status: ", user.__getattribute__("status"))
 
+    photo = client.download_profile_photo(username)
+    print("photo location: ", photo)
 
-def sign_in():
-    pass
-    # client.send_message('1286407260', 'Hello! Talking to you from Telethon')
-    # client.download_profile_photo('ijustwannatestthis')
-    # messages = client.get_messages('username')
-    # messages[0].download_media()
-    #
-    # @client.on(events.NewMessage(pattern='(?i)hi|hello'))
-    # async def handler(event):
-    #     await event.respond('Hey!')
+    print("bio: ", entity.__getattribute__("about"))
 
 
 def main():
-    print("Please enter telegram username:")
-    username = 'ijustwannatestthis'
+    username = input('Please enter telegram username:')
     harvest(username)
 
 
