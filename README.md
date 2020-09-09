5362f8acb775f6047ae5b2d35a18ce8208f52081
ghazale <ghazalnoroozi27@yahoo.com>
2020-09-09 22:33:59 +0430

    Refactored README

--- README.md
+++ README.md
@@ -6,36 +6,16 @@ This project is a simple code for retrieving a telegram user information by its 
 
 These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
 
-### Prerequisites
-
-You need python3 installed on your system.
-I am using the Python version 3.7.3. You can see your python version with the command below:
-```
->python --version
-Python 3.7.3
-```
-
 ### Installing
 
-A step by step series of examples that tell you how to get a development env running
-
-Say what the step will be
-
-```
-Give the example
-```
-
-And repeat
-
-```
-until finished
-```
-
-End with an example of getting some data out of the system or using it for a little demo
+Use [isntall.sh](https://github.com/GhazaleNoroozi/EmailValidation/blob/master/install.sh) file for installation on linux.
 
 ## Running the tests
 
-Explain how to run the automated tests for this system
+For running the tests cd in the project directory and run command below:
+```
+python -m unittest tests/test.py
+```
 
 ## Built With
 
---
+++ tests/test.py
@@ -0,0 +1,35 @@
+import unittest
+from src.Main import *
+
+client = create_client()
+
+
+class Config(unittest.TestCase):
+
+    def test_username_valid(self):
+        info = harvest_by_username(client, 'ghazale_nrz')
+        self.assertEqual(info['username'], 'ghazale_nrz')
+        self.assertEqual(info['phone'], '989388488825')
+        self.assertEqual(info['first_name'], "Ghazale")
+        self.assertEqual(info['bio'], 'bio')
+
+    def test_username_not_valid(self):
+        # Channel
+        self.assertEqual(harvest_by_username(client, 'zahra'),
+                         'This username either belongs to a channel or a group')
+        # Group
+        self.assertEqual(harvest_by_username(client, 'hi_group'),
+                         'This username either belongs to a channel or a group')
+        # None-existent username
+        self.assertEqual(harvest_by_username(client, 'sdlkjflasjf'),
+                         'This username does not exist')
+        # Phone number with no telegram account
+        self.assertEqual(harvest_by_phone(client, '+989372639717'),
+                         'There is no account connected to this phone number')
+        # Nonsense
+        self.assertEqual(harvest_by_phone(client, 'johowfoiej'),
+                         'There is no account connected to this phone number')
+
+
+if __name__ == '__main__':
+    unittest.main(client)
