6702eed39aa21f0a28b828f7ab5cacaeccc2c4b1
ghazale <ghazalnoroozi27@yahoo.com>
2020-09-10 02:37:35 +0430

    Refactor methods and Add docstring

--- src/Main.py
+++ src/Main.py
@@ -1,10 +1,23 @@
 from telethon.sync import TelegramClient
 from telethon.tl.functions import users
 from telethon.tl.types import User, Channel
-import Config
+from src import Config
+import json
+
+
+def x_str(s):
+    if s is None:
+        return 'UNAVAILABLE'
+    return str(s)
 
 
 def harvest_user(client, entity):
+    """
+    Retrieve and print information
+    :param client: Telegram client
+    :param entity: The target entity
+    :return: None
+    """
     user = entity.__getattribute__("user")
     username = user.__getattribute__("username")
     photo_location = client.download_profile_photo(username)
@@ -17,27 +30,32 @@ def harvest_user(client, entity):
     phone = user.__getattribute__("phone")
     status = user.__getattribute__("status")
 
-    print('username: ', username,
-          '\nid: ', tid,
-          '\naccess_hash: ', access_hash,
-          '\nis_bot: ', is_bot,
-          '\nfirst_name: ', first_name,
-          '\nlast_name: ', last_name,
-          '\nphone number: ', phone,
-          '\nbio: ', about,
-          '\nstatus: ', status,
-          '\nphoto file location: ', photo_location
-          )
+    info = '{"username": "' + x_str(username)\
+           + '", "phone": "' + x_str(phone)\
+           + '", "first_name": "' + x_str(first_name)\
+           + '", "last_name": "' + x_str(last_name)\
+           + '", "bio": "' + x_str(about)\
+           + '", "last seen": "' + x_str(status)\
+           + '", "photo": "' + x_str(photo_location) + '"}'
+
+    json_info = json.loads(info)
+    print(type(json_info))
+    return json_info
 
 
 def harvest_by_phone(client, phone):
+    """
+    Harvest user's information by phone number of there is an account connected to the phone number
+    :param client: Telegram client
+    :param phone: Target user phone number
+    :return: None
+    """
     try:
         entity = client(users.GetFullUserRequest(id=phone))
     except ValueError:
-        print("There is no account connected to this phone number.")
-        return
+        return 'There is no account connected to this phone number'
 
-    harvest_user(client, entity)
+    return harvest_user(client, entity)
 
 
 def harvest_by_username(client, username):
@@ -45,23 +63,27 @@ def harvest_by_username(client, username):
     Print information about the target user by creating a client
     :param client: The host client
     :param username: Target username
+    :return None
     """
     try:
         entity = client.get_entity(username)
     except ValueError:
-        print("This username does not exist")
-        return
+        return 'This username does not exist'
 
     if type(entity) == User:
         entity = client(users.GetFullUserRequest(id=username))
-        harvest_user(client, entity)
+        return harvest_user(client, entity)
     elif type(entity) == Channel:
-        print("This username either belongs to a channel or a group")
+        return 'This username either belongs to a channel or a group'
     else:
-        print("This username does not belong to  a user, bot, channel or a group")
+        return 'This username does not belong to  a user, bot, channel or a group'
 
 
-def main():
+def create_client():
+    """
+    Create and connect a telegram client using information in Config.py file
+    :return: Created client
+    """
     host_api_id = Config.api_id
     host_api_hash = Config.api_hash
     host_user_id = Config.user_id
@@ -72,12 +94,23 @@ def main():
     if not client.is_user_authorized():
         client.send_code_request(host_phone)
         client.sign_in(host_phone, input('Enter code sent to your telegram: '))
+    return client
 
+
+def main():
+    """
+    Main method
+    :return: None
+    """
+
+    client = create_client()
     code = input('Do you want to find the user based on username or phone number? [u/p] ')
     if code == 'u':
-        harvest_by_username(client, input('Enter username: '))
+        res = harvest_by_username(client, input('Enter username: '))
+        print(res)
     else:
-        harvest_by_phone(client, input('Enter phone number (don\'t forget the country code): '))
+        res = harvest_by_phone(client, input('Enter phone number (don\'t forget the country code): '))
+        print(res)
 
 
 if __name__ == '__main__':
